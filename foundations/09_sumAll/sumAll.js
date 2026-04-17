916d31c9aa571a2c83767bcc0c26cdff941dd3f4
Rectro9 <j9namayanjr@gmail.com>
2026-04-17 07:08:27 -0700

    Complete sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    let start = Math.min(a, b)
+    let end = Math.max(a, b)
 
+    if(!Number.isInteger(a) || (a) < 0 || Array.isArray(a)){
+        return "ERROR"
+    }
+    else if(!Number.isInteger(b) || (b) < 0 || Array.isArray(b)){
+        return "ERROR"
+    }
+
+    for (let i = start; i <= end; i++){
+        sum += i
+    }
+
+    return sum
 };
 
 // Do not edit below this line
