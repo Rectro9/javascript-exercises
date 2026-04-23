52275dd54ad34761f36c0a47a067b45e84684489
Rectro9 <j9namayanjr@gmail.com>
2026-04-23 09:16:44 -0700

    Complete tempConversion exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let formula = ((num - 32) * 5/9).toFixed(1)
+  let result = Number(formula)
+  return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let formula = ((num * 1.8) + 32).toFixed(1)
+  let result = Number(formula)
+  return result
 };
 
 // Do not edit below this line
