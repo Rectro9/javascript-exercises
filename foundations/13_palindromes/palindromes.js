cdf027c16bd9f959d8588e793faed37f94a9342a
Rectro9 <j9namayanjr@gmail.com>
2026-04-28 15:07:47 -0700

    Complete palindrome exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (array) {
+    let cleanArray = (array.replace(/[^a-zA-Z0-9]/g, "")).toLowerCase();
+    let rightSideArray = cleanArray.length - 1;
 
+    for(let i = 0; i < cleanArray.length / 2; i++){
+        if(cleanArray[i] !== cleanArray[rightSideArray]){
+            return false;
+        }
+        rightSideArray--;
+    }
+    return true;
 };
 
 // Do not edit below this line
