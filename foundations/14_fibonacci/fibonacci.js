170e85f43bd7fd521ef171aded303384fe1b2069
Rectro9 <j9namayanjr@gmail.com>
2026-05-05 05:30:24 -0700

    Complete fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let previous1 = 0;
+    let previous2 = 1;
+    let current = 0;
+    let a = Number(n);
+
+    if(a === 1 || a === 2){
+        return 1;
+    }
+    else if(a === 0){
+        return 0;
+    }
+    else if(a < 0){
+        return "OOPS";
+    }
+
+    for(let i = 1; i < a; i++){
+        current = previous1 + previous2 
+        previous1 = previous2
+        previous2 = current
+    }
+    return current;
 
 };
 
