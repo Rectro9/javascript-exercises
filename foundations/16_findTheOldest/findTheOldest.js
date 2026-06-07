485ada69e2f49e61f1091541b33a3a3b8fe195fe
Rectro9 <j9namayanjr@gmail.com>
2026-06-06 23:14:30 -0700

    Complete findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((accumulate, current) => {
+        const missingYear = new Date().getFullYear()
 
+        let accumulateAge = accumulate.yearOfDeath - accumulate.yearOfBirth
+        let currentAge = current.yearOfDeath - current.yearOfBirth
+
+        if(!accumulate.yearOfDeath){
+            accumulateAge = missingYear - accumulate.yearOfBirth
+        }
+        
+        if(!current.yearOfDeath){
+            currentAge = missingYear - current.yearOfBirth
+        }
+
+        if(currentAge > accumulateAge){
+            return current
+        }
+        else {
+            return accumulate
+        }
+    })
 };
 
 // Do not edit below this line
